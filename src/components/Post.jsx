8b211b23814be3849de9a9dd64f62db15e2ab057
Nicholas <nicholasjoven@gmail.com>
2023-01-14 18:02:16 -0500

    Switching to vercel

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,7 +6,7 @@ const Post = () => {
     const params = useParams()
     
     const file = useMemo(() => {
-        return `${params.title}.md`
+        return `$/${params.title}.md`
     }, [params])
     console.log(file)
     return (
