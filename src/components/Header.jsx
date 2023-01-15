f419f67ef77ba495f1bba8cf6947a4aa09ef3df3
Nicholas <nicholasjoven@gmail.com>
2023-01-14 19:38:40 -0500

    Added absolute path to images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,16 +10,16 @@ const Header = () => {
         <div className='header'>
             <div className='header-icons'>
                 <a className='redirect' href='https://github.com/nickjoven'>
-                    <img className='icon' src='./githubwhite.png' />
+                    <img className='icon' src='https://nickjoven.github.io/blog/githubwhite.png' />
                 </a>
                 <a className='redirect' href='https://www.linkedin.com/in/nick-joven/'>
-                    <img className='icon' src='./linkedinwhite.png' />
+                    <img className='icon' src='https://nickjoven.github.io/blog/linkedinwhite.png' />
                 </a>
                 <a className='redirect' href='https://nickjoven.github.io/earworm-react/'>
-                    <img className='icon' src='./eighth.png' />
+                    <img className='icon' src='https://nickjoven.github.io/blog/eighth.png' />
                 </a>
                 <div className='redirect' onClick={handleClick}>
-                    <img className='icon' src='./house.png' />
+                    <img className='icon' src='https://nickjoven.github.io/blog/house.png' />
                 </div>
             </div>
         </div>
