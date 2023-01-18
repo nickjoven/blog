17e346354dd47eb302a3db5af329eedc67c75543
Nicholas <nicholasjoven@gmail.com>
2023-01-18 00:26:45 -0500

    added syntax highlighting

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,8 +1,30 @@
 import { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import rehypeRaw from 'rehype-raw'
 import PostFooter from './PostFooter'
+
+const CodeBlock = {
+    code({ node, inline, className, children, ...props }) {
+        const match = /language-(\w+)/.exec(className || '')
+        return !inline && match ? (
+            <SyntaxHighlighter
+                style={dracula}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+            >
+                {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+        ) : (
+            <code className={className} {...props}>
+                {children}
+            </code>
+        )
+    },
+}
 
 const Markdown = ({ file }) => {
     const [content, setContent] = useState('')
@@ -30,7 +52,7 @@ const Markdown = ({ file }) => {
                 null
             ) : (
                 <div className='post'>
-                    <ReactMarkdown rehypePlugins={[rehypeRaw]} children={content} />
+                    <ReactMarkdown rehypePlugins={[rehypeRaw]} children={content} components={CodeBlock} />
                     <PostFooter />
                 </div>
             )}   
